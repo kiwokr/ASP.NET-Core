1bc1e97006f48ceb14c87eb69daf7d8a5718b085
Krasnopeev Kirill <kirill.krasnopeev@gmail.com>
2018-07-27 17:12:16 +0300

    add

--- MobileStore/Controllers/HomeController.cs
+++ MobileStore/Controllers/HomeController.cs
@@ -19,5 +19,21 @@ namespace MobileStore.Controllers
         {
             return View(db.Phones.ToList());
         }
+
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
     }
+
 }
